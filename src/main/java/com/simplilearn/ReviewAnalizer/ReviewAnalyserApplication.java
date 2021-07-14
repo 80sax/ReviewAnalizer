8efcc8b22e24c1b81679173e1231da9b397928df
Abraham <asoteloa@outlook.com>
2021-07-14 15:43:36 -0500

    Fix

--- src/main/java/com/simplilearn/ReviewAnalizer/ReviewAnalyserApplication.java
+++ src/main/java/com/simplilearn/ReviewAnalizer/ReviewAnalyserApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewAnalyserApplication {
-  
+public class ReviewAnalyserApplication {  
   public static double getWordCount(String review){
     int count = 0;
     String string[] = review.toLowerCase().split("([,.\\s]+)");
-
     for(String s : string){
       count++;
     }
